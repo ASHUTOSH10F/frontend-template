0a4294783b2b1ab0d3ed1102a8b046167c7d6cd0
ASHUTOSH10F <117303799+ASHUTOSH10F@users.noreply.github.com>
2024-03-09 23:27:40 +0530

    frontend(9/3/24)

---
+++ learn.js
@@ -0,0 +1,195 @@
+var Name="Ashutosh";
+var Age=22;
+var City="Gonda";
+var State="Uttar Predesh";
+var IsStudent="True";
+
+
+// console.log("My Name is:"+ Name)
+// console.log("Age is",22)
+// console.log("Liveing in",City)
+// console.log("It's is Under:",State)
+// console.log("Curentlly He is Student",IsStudent);
+
+
+// console.log(
+//     `
+//     My name is ${Name}
+//     My currnt Age ${Age}
+//     I am liveing in city ${City}
+//     it's stablish in state ${State}
+//     is it student currentlly ? ${IsStudent}
+//     `
+// )
+// var f= 50;
+// function sum(a,b)
+// {
+//      var d=20;
+//     var c= a+b;
+//     console.log(`Sum of Two Number is ${c}`);
+// }
+// sum(10,2);
+// console.log(f);
+
+
+// var a=20;
+// a=String(5);
+// console.log(a);
+
+
+// alert("Hii My name is Ashutosh mishra");
+
+// prompt("Can You here Me");
+// confirm("Hii My self Ashutosh");
+
+
+// console.log(a);
+// var a= 20;
+
+// // console.log(b)
+// let b =10;
+// console.log(b);
+
+// // console.log(c);
+// const c =40;
+// console.log(c);
+
+
+// var a= 20;
+// var a= 30;
+// console.log(b);
+
+// let b= 40;
+// console.log(b);
+// // let b= 50;
+
+// const c =60;
+// console.log(c);
+// // const c =70;
+
+
+var arr = [10, 20, 30, 40, 50, 60];
+// console.log(arr);
+
+
+// console.log(arr.push(70));
+// console.log(arr);
+
+// console.log(arr.pop());
+// console.log(arr);
+
+// console.log(arr.shift(80));
+// console.log(arr);
+
+// console.log(arr.unshift());       
+// console.log(arr);
+
+// console.log(arr.slice(1,2));
+// console.log(arr);
+
+ 
+// var arr2 = [10, 20, 30, 40, 50]
+// console.log(arr2)
+// console.log(arr.slice(1,3));
+// console.log(arr2);
+
+
+// var obj = {
+//     name:"Ashutosh Mishra",
+//     Age:20,
+//     State:"Uttar pradesh"
+// }
+// console.log(obj);
+// console.log(obj.name);
+
+
+// (function()
+// {
+// console.log(`This is immdiate invoke function`);
+// }())
+
+
+
+// function sum(a,b)
+// {
+//     var result = a+b;
+//     console.log(result);
+// } 
+// sum(10,20);
+
+// var sum2 = (a,b) => (a+b);
+
+// console.log(sum2(20,30));
+
+
+// var arr = [10,20,30,40,50,60];
+// console.log(arr);
+// console.log(arr.length);
+
+
+// console.log(arr.slice(1,4));
+// console.log(arr.length);
+// console.log(arr);
+
+//  var arr1 = ["Hello My Name is Ashutosh Mishra"];
+// console.log(arr1.slice(4,7));
+
+
+// var arr = [10,20,30,40,50,60,70,80,90];
+// console.log(arr);
+// arr.splice(3,0,50.5);
+// console.log(arr);
+
+// let obj_1 = {
+//     name:"Ashutosh",
+//     Skills:"frontEnd"
+// }
+
+// let obj_2= obj_1;
+// console.log(obj_2);
+// console.log(obj_2.name="Mishra")
+// console.log(obj_1);
+// console.log(obj_2);
+
+// var b= 30;
+
+// console.log(A);
+// console.log(b);
+
+// var arr = [10,20,30,40];
+// console.log(arr[2]) // here its out is 30
+
+// //  from destrcturing  
+
+// let[first,second,thred,fourth]= arr;
+
+// alert(first);
+
+
+// var obj ={
+//     Name:"Ashutosh",
+//     Age:22,
+//     City:"Gonda"
+// }
+// console.log(obj);
+
+// obj[FullName,Age,City]=obj;
+// console.log(obj.Age);
+
+let my_name ="hello";
+console.log(my_name);
+ 
+
+
+
+// let color = COLOR_ORANGE;
+// console.log(color);
+
+
+let color = COLOR_ORANGE;
+alert(color); // #FF7F00
+
+
+
+
+
